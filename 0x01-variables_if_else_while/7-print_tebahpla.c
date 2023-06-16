dc394102bdf11454078a11164d6b669bfc3a9af4
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-16 19:55:50 +0000

    Mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include stdio.h
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+	int a = 97;
+
+	for (int i = a + 25; i >= a; i++)
+	{
+		putchar(i)
+	}
+
+	putchar('\n');
+
+	return (0);
+}
