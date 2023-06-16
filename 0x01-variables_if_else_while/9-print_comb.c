52ff94a8cb20c7afba21576c69f0690b5aaeedc4
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-16 22:22:36 +0000

    Characters

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success
+ */
+int main(void)
+{
+	putchar('0')
+
+		for (int i = 1; i < 10; i++)
+		{
+			putchar(',');
+			putchar(' ');
+			putchar('0' + i);
+		}
+
+	putchar('\n');
+	
+	return (0);
+}
