1ad735ab10ae60a04810dd6a159bf0794ed73596
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-16 17:23:57 +0000

    Upper/Lower case ALPHABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ *
+ * Return: 0 indicate success.
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z')
+	{
+		putchar(letter);
+	}
+
+	for (letter ='A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
