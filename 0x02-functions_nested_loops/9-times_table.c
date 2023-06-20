1c153e2ec9da7b6253157627b7b14a4bb24df5a8
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-20 23:07:39 +0000

    Updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,23 +11,24 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (j != 0)
+
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((result / 10) + '0');
+			}
+
+			_putchar((result % 10) + '0');
+
+			if (j !=9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (result < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (result >= 10 && i != 0)
-			{
-				_putchar(' ');
-			}
-			_putchar(result / 10 + '0');
-			_putchar(result % 10 + '0');
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
