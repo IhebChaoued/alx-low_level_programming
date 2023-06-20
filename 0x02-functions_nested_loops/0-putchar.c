1bfc2f6119d1bc52590fe3903baf927cbcd52d5b
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-20 13:47:50 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,22 @@
  *
  * Return: 0 indicate success.
  */
-int main(void)
-{
-	puts("_putchar\n");
+int main(void) {
+
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
+}
+
+int _putchar(char c) {
+	
+	return putchar(c);
 }
