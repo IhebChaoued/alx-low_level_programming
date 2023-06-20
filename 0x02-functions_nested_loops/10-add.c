46fdee7f4da6e02099f1ec40c72e10acf15c787f
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-20 23:37:36 +0000

    Updated

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * print_number - Prints an integer using _putchar
- * @n: The integer to print
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of the two integers.
  */
 int add(int a, int b)
 {
 	int result = a + b;
-	return result;
+	return (result);
 }
