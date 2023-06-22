95e83d9e33f672db5a5a1da285232b2c04c3485a
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-22 17:17:55 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * FizzBuzz test program.
+ * Prints numbers from 1 to 100 with special conditions.
+ * For multiples of three, prints "Fizz".
+ * For multiples of five, prints "Buzz".
+ * For multiples of both three and five, prints "FizzBuzz".
+ * Each number or word is separated by a space.
+ */
+int main(void)
+{
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf('\n');
+	return (0);
+}
