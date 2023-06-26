0e900298239625175dbebeb0d67d2d73105ec61e
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-26 23:19:43 +0000

    Print char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,15 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i, len;
 
-	while (str[i] != '\0')
+	len = strlen(str);
+	for (i = 0; i < len; i++)
 	{
-		_putchar(str[i]);
-		i += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
-
 	_putchar('\n');
 }
