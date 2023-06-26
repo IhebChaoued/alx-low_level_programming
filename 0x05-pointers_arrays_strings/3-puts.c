16431a161fba3a25520665c39896f13dba9c5b33
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-26 17:13:10 +0000

    stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - Prints a string to stdout followed by a newline.
+ * @str: Pointer to the string to be printed.
+ */
+void _puts(char *str)
+{
+	fputs(str, stdout);
+	putchar('\n');
+}
