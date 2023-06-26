cd696bc2e5ecdefc0a6fcdeefbcdbed72fa5c994
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-26 23:29:04 +0000

    Half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,24 +7,13 @@
  */
 void puts_half(char *str)
 {
-	int length = strlen(str);
-	int start_index;
+	int i, len, half;
 
-	if (length % 2 == 0)
-	{
-		start_index = length / 2;
-	}
-	else
-	{
-		start_index = (length - 1) / 2;
-	}
-
-	int i;
-
-	for (i = start_index; i < length; i++)
-	{
+	len = strlen(str);
+	half = len / 2;
+	if (len % 2 != 0)
+		half = half + 1;
+	for (i = half; i < len; i++)
 		_putchar(str[i]);
-	}
-
 	_putchar('\n');
 }
