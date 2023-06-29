4952787c577ae66ce303e27b96ada6ceac437349
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-29 14:21:35 +0000

    ttoupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - Converst all lowercase letters to uppercase
+ * @str: the string to be modified.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (str[i] != \0)
+	{
+		str[i] = toupper(str[i]);
+		i++;
+	}
+	return (str);
+}
