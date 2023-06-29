22ce5a1722fca8864f770cd5e9d33ee549f70782
Iheb Chaoued <ihebchaoued@gmail.com>
2023-06-29 14:26:47 +0000

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * string_toupper - Converst all lowercase letters to uppercase
+ * *string_toupper - Converst all lowercase letters to uppercase
  * @str: the string to be modified.
  *
  * Return: A pointer to the modified string.
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
 	int i = 0;
 
