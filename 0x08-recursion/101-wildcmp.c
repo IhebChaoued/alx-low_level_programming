cff26332949da4e47478284400f17df6ed2d539f
Iheb Chaoued <ihebchaoued@gmail.com>
2023-07-05 15:07:11 +0000

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * wildcmp - Compares two strings and checks if they can be considered identical.
+ * wildcmp - Compares two strings.
  * @s1: The first string to compare.
  * @s2: The second string to compare.
  *
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 		if (*s1 == '\0')
 			return (wildcmp(s1, s2 + 1));
 
-		return (wildcmp(s1 + 1, s2)) || wildcmp(s1, s2 + 1);
+		return (wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1));
 	}
 
 	if (*s1 != *s2)
