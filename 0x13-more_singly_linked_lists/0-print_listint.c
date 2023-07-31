a344b9c4b99bf5e2fbb80489be9f4c4c14013c7d
Iheb Chaoued <ihebchaoued@gmail.com>
2023-07-31 16:13:05 +0000

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints the elements of a linked list of integers.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
+ */
+ize_t print_listint(const listint_t *h)
+{
+	struct listint_t *head = NULL;
+
+	head = (struct listint_t *)malloc(sizeof(struct listint_t));
+
+	head->data = 8;
+	head->link = NULL;
+
+	printf("%d", head->data, head->link);
+}
