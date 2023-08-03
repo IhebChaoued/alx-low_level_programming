cd6f6a910bd29afdd719ee9e28dadca35e8d5b89
Iheb Chaoued <ihebchaoued@gmail.com>
2023-08-03 17:57:07 +0000

    set bit to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,7 +12,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
+	unsigned long int mask = 1L << index;
 	*n |= mask;
 
 	return (1);
