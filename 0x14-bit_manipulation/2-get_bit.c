2f8d8a7cc4ee0ce5c2bc8f39f47b56f29fe92efd
Iheb Chaoued <ihebchaoued@gmail.com>
2023-08-04 01:25:17 +0000

    value of bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,8 +7,9 @@
  *
  * Return: The value of the bit at the given index or -1 if an error occurred.
  */
-int get_bit(unsigned long int n, unsigned int index) {
-	if (index >= sizeof(unsigned long int) * 8) 
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	unsigned long int mask = 1UL << index;
