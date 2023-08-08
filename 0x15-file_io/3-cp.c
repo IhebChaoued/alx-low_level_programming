46b2065ee6695cbb29b321942c04110e78263e91
Iheb Chaoued <ihebchaoued@gmail.com>
2023-08-08 01:08:29 +0000

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,14 +1,24 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program.
+ * print_error_and_exit - Print an error message and exit the program.
  *
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- *
- * Return: (0) on success, (exit code) on failure.
+ * @exit_code: The exit code to be used when exiting.
+ * @message: The error message to be printed.
  */
-int main(int argc, char **argv)
+void print_error_and_exit(int exit_code, const char *message)
+{
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(exit_code);
+}
+
+/**
+ * copy_file - Copy content from file_from to file_to.
+ *
+ * @file_from: The source file path.
+ * @file_to: The destination file path.
+ */
+void copy_file(const char *file_from, const char *file_to)
 {
 	int file_from_fd = open(file_from, O_RDONLY);
 	int permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
@@ -16,30 +26,43 @@ int main(int argc, char **argv)
 	char buffer[BUFFER_SIZE];
 	ssize_t bytes_read, bytes_written;
 
-	if (argc != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
-
 	if (file_from_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD), exit(98);
 
 	if (file_to_fd == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+		dprintf(STDERR_FILENO, ERR_NOWRITE), exit(99);
 
 	while ((bytes_read = read(file_from_fd, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(file_to_fd, buffer, bytes_read);
 		if (bytes_written != bytes_read)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, argv[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE), exit(99);
 	}
 
 	if (bytes_read == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, argv[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD), exit(98);
 
 	if (close(file_from_fd) == -1)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE), exit(100);
 
 	if (close(file_to_fd) == -1)
 		dprintf(STDERR_FILENO, ERR_NOCLOSE), exit(100);
+}
 
-	return (EXIT_SUCCESS);
+/**
+ * main - Entry point of the program.
+ *
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the command-line arguments.
+ *
+ * Return: (0) on success, (exit code) on failure.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+		dprintf(STDERR_FILENO, USAGE), exit(97);
+
+	copy_file(argv[1], argv[2]);
+
+	return (0);
 }
