7fd050ca43e12ac506e11e653c4b755c3fcdb32c
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-03 15:19:21 +0000

    lib

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,6 +44,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 /* Task 3 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+hash_node_t *new_node(const char *key, const char *value);
 
 /* Task 4 */
 char *hash_table_get(const hash_table_t *ht, const char *key);
