8fbc8d3fcf35bee66fc5a2852a24a077c1c635f4
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-06 15:10:42 +0000

    updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,9 +15,8 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t low, size_t high, int value);
-void print_subarray(int *array, size_t low, size_t high);
 int advanced_binary(int *array, size_t size, int value);
-
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 /* Data structure definition for Jump Search in a singly linked list */
 typedef struct listint_s
 {
