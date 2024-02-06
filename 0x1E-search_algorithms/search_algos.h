eff0273f993232edbf571b291a1633c4f3907bc4
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-06 15:16:34 +0000

    updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -37,5 +37,8 @@ typedef struct skiplist_s
 /* Function prototypes for advanced search algorithms in linked lists */
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
 
 #endif /* SEARCH_ALGOS_H */
