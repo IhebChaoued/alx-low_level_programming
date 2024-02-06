285c9ea7e157956b1a78f9717a19767474754ae6
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-06 15:19:49 +0000

    Lib

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,44 +1,56 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
 
-#include <stdio.h>
+/* LIBRARIES */
 #include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 
-/* Function prototypes for basic search algorithms */
+/* STRUCTURES */
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+/* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void print_array(int *array, size_t left, size_t right);
-
-/* Function prototypes for advanced search algorithms */
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t low, size_t high, int value);
 int advanced_binary(int *array, size_t size, int value);
-int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
-/* Data structure definition for Jump Search in a singly linked list */
-typedef struct listint_s
-{
-    int n;
-    size_t index;
-    struct listint_s *next;
-} listint_t;
-
-/* Data structure definition for Interpolation Search */
-typedef struct skiplist_s
-{
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
-} skiplist_t;
-
-/* Function prototypes for advanced search algorithms in linked lists */
 listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
 listint_t *create_list(int *array, size_t size);
 void free_list(listint_t *list);
 void print_list(const listint_t *list);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif /* SEARCH_ALGOS_H */
+#endif
