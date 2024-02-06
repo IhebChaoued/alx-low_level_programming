6a64074151cecfb7ea9328cf2d8c28e50c0f1a43
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-06 14:53:00 +0000

    Header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,10 +3,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/* Function prototypes */
+/* Function prototypes for basic search algorithms */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t left, size_t right);
+
+/* Function prototypes for advanced search algorithms */
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+/* Data structure definition for Jump Search in a singly linked list */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+/* Data structure definition for Interpolation Search */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+/* Function prototype for Jump Search in a singly linked list */
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+/* Function prototype for Linear Search in a skip list */
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif /* SEARCH_ALGOS_H */
