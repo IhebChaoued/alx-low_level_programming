3cb3e719018fa8c54e1798784726791a69e89c57
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-06 14:59:26 +0000

    Header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,7 +13,11 @@ void print_array(int *array, size_t left, size_t right);
 /* Function prototypes for advanced search algorithms */
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+
 int exponential_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t low, size_t high, int value);
+void print_subarray(int *array, size_t low, size_t high);
+
 int advanced_binary(int *array, size_t size, int value);
 
 /* Data structure definition for Jump Search in a singly linked list */
