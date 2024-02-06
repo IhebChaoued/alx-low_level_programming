53e5a013228eca9b093d5456c5f786147d54823e
Iheb Chaoued <ihebchaoued@gmail.com>
2024-02-06 16:13:31 +0000

    skip*

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip - Linear skip search in a sorted singly linked list.
+ * @list: Pointer to the head of the linked list
+ * @value: Value to search for
+ *
+ * Return: Pointer to the first node if found, else NULL
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *jump;
+
+	if (!list)
+		return (NULL);
+
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n", node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
